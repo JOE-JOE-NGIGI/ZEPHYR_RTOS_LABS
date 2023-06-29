212ba238f0855d64ea98c096cc80ad6f33e8f636
JOE-JOE-NGIGI <jngigiw@gmail.com>
2023-06-29 12:16:41 +0300

    Add installation commands

--- installation_script.sh
+++ installation_script.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+if [[ -f "kitware-archive.sh"]]
+then
+    echo "file exists on your filesystem."
+else
+    wget https://apt.kitware.com/kitware-archive.sh
+fi
 
-wget https://apt.kitware.com/kitware-archive.sh
 sudo bash kitware-archive.sh
 
 #Install required dependencies
@@ -13,3 +18,23 @@ sudo apt install --no-install-recommends git cmake ninja-build gperf \
 cmake --version
 python3 --version
 dtc --version
+
+#install west and make sure ~/.local/bin is on your PATH env variable
+pip3 install --user -U west
+echo 'export PATH=~/.local/bin:"$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+#Get Zephyr source code
+mkdir ~/zephyrproject
+echo "zephyrproject directory created in home directory"
+
+#Get the zephyr source code
+west init ~/zephyrproject
+cd ~/zephyrproject
+west update
+
+#export a zephyr CMake package
+west zephyr-export
+
+#installing additional python dependencies
+pip3 install --user -r ~/zephyrproject/zephyr/scripts/requirements.txt
